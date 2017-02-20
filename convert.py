1b9cf222cd64a2c667a26b39f8c13ce76e2974f2
sorenkagawa <coreyclip@gmail.com>
2017-02-20 23:28:28 +0100

    fresh approach to standardizer
    A fresh start on the standardizer, uses openpyxl library to read raw excel sheet. Currently the script only deals with reading in the meta data and tries to output it to a csv file. Currently there is one error in the code: 
    
    UnicodeEncodeError                        Traceback (most recent call last)
    <ipython-input-31-9355f74c4cc5> in <module>()
         61                     'Emri i menaxherit t‘ prokurimit': ws['I18'].value,
         62                     'E-mail adresa': ws['I19'].value,
    ---> 63                     'Adresa e webit t‘ AK' : ws['I20'].value
         64                     })
         65 
    //anaconda/lib/python2.7/csv.pyc in writerow(self, rowdict)
        150 
        151     def writerow(self, rowdict):
    --> 152         return self.writer.writerow(self._dict_to_list(rowdict))
        153 
        154     def writerows(self, rowdicts):
    UnicodeEncodeError: 'ascii' codec can't encode character u'\xeb' in position 17: ordinal not in range(128)
    
    I'm not sure where I can add in a specification to encode using utf-8

--- convert.py
+++ convert.py
@@ -1,168 +1,66 @@
-import numpy as np
-import pandas as pd
-filepath = '/2015-raporti-vjetor-per-kontratat-e-nenshkruara-publike.xlsx'
-df = pd.read_excel(filepath, skiprows=26, skip_footer=58, index_col=False)
-# pull meta data
-meta = pd.read_excel(filepath, skiprows=0, skip_footer=173, index=False, encoding='iso8859_2')
-publishedDate = pd.Series(pd.to_datetime(meta.iat[5,6]))
-publisher = pd.Series(meta.iloc[:,8].str.encode(encoding="utf-8", errors="strict"))
-publisher_fields = pd.Series(meta.iloc[:,1]).str.encode(encoding="utf-8", errors="strict")
+import openpyxl, pprint
 
-meta_info = pd.concat([publisher_fields, publisher], ignore_index=True, axis=1)
-meta_info.iat[5,1] = publishedDate
-meta_info.rename(columns={0:"publisher_fields",1:'publisher_info'}, inplace=True)
-print meta_info
+filepath = '/Users/coreyclip/Desktop/2015-raporti-vjetor-per-kontratat-e-nenshkruara-publike.xlsx'
+wb = openpyxl.load_workbook(filepath)
+ws = wb.get_active_sheet()
 
+#gather meta data
+d = defaultdict(dict)
+d['Data e p‘rgatitjes s‘ raportit'] = ws['G8'].value
 
-#rename columns to comply with OCDS
-df.rename(index=str,
-    columns={
-        df.rename(index=str, columns={
-            1:'releases/planning/budget/source', 
-            2:'id', 
-            3:'releases/tender/description', 
-            4:'releases/tender/value/description', 
-            5:'releases/tender/procurementMethod', 
-            6:'releases/FPP', 
-            7:'releases/tender/title', 
-            8:'releases/planning/period/endDate', 
-            9:'releases/tender/tenderPeriod/startDate',
-            10:'releases/tender/tenderPeriod/endDate', 
-            11: 'releases/contract/DateSigned',
-            12:'releases/contract/contractPeriod/timeframe', 
-            13:'releases/contract/contractPeriod/endDate', 
-            14:'releases/contract/contractValue/amount/estimate', 
-            15:'releases/contract price', 
-            16:'releases/Annex/AnnexValue/amount',
-            17:'releases/Contract/contractValue/amount/deductions', 
-            18:'releases/contract/contractValue/amount', 
-            19:'releases/Award/AwardSuppliers/name', 
-            20: 'releases/Award/AwardSuppliers/local',
-            21:'releases/Tender/numberOfEnquires', 
-            21:'releases/Tender/numberOfRequests', 
-            'Unnamed: 21': 'releases/Tender/numberOfTenderers',
-            22: 'releases/Tender/numberOfTendersRejected', 
-            23:'releases/Tender/details/expedited', 
-            24:'releases/Tender/awardCriteria'
-        })},
-    inplace=True)
+d['Viti fiskal'] = ws['P8'].value
 
-#add in currency information
-df['Value.currency'] = 'EUR'
-#add in language code
-df['langauge'] = 'en'
-#add OCID prefix
-df['ocid'] = 'ocds-3n5h6d-'
-df['id'] = df['id'].astype(str)
-df['ocid'] = df[['ocid','id']].apply(lambda x: ''.join(x), axis=1)
+d['Emri zyrtar i Autoritetit Kontraktues'] = ws['I12'].value
 
+d['Lloji i Autoritetit Kontraktues (zgjidh‘ne nj‘ren)'] = ws['I13'].value
 
+d['Kodi buxhetor'] = ws['I14'].value
 
-###############################
+d['Adresa'] = ws['I15'].value
 
+d['Kodi postar - Qyteti'] = ws['I16'].value
 
-#transform datetime formats
+d['Tel. fiks/ Celulari /Faksi'] = ws['I17'].value
 
-def to_date(series):
-    ts = pd.to_datetime(series, yearfirst = True)
-    return ts
-    
-startTime = str('00:00:00UTC+1h')
-endTime= str('23:23:59UTC+1h')
+d['Emri i menaxherit t‘ prokurimit'] = ws['I18'].value
 
-df['planning/period/endDate'] = to_date(df['planning/period/endDate'])
-df['tender/tenderPeriod/startDate' ] = to_date(df['tender/tenderPeriod/startDate' ])
-df['tender/tenderPeriod/endDate'] = to_date(df['tender/tenderPeriod/endDate'])
-df['contract/DateSigned'] = to_date(df['contract/DateSigned'])
-df['contract/contractPeriod/endDate'] = to_date(df['contract/contractPeriod/endDate'])
-    
-#convert numerical variables back into string text
+d['E-mail adresa'] = ws['I19'].value
 
-df['planning/budget/source'] = df['planning/budget/source'].replace([1,2,3],['local municipal funds', 'Kosovo Consolidated Budget', 'Donation' ])
-df['tender/description'] = df['tender/description'].replace([1,2,3,4,5,6,7], ['supply', 'services', 'counseling services','design contest', 'jobs', 'concession jobs', 'immovable property'])
-df['tender/value/description'] = df['tender/value/description'].replace([1,2,3,4], ['great','medium','small', 'minimal'])
-df['tender/procurementMethod'] = df['tender/procurementMethod'].replace([1,2,3,4,5,6,7],['open procedure', 'restricted procedure', 'design contest', 'negotiated procedure after publication of a contract notice','negotiated procedure without publication of a contract notice', 'price quotation procedure', 'cheapest offer' ])
-df['Award/AwardSuppliers/local'] = df['Award/AwardSuppliers/local'].replace([1,2],['domestic', 'international'])
+d['Adresa e webit t‘ AK'] = ws['I20'].value
 
+import csv 
+outputfile = open('/Users/coreyclip/Desktop/meta_data.csv', 'w')
+with open(outputfile, 'w') as csvfile:
+    fieldnames = [
+                'Data e p‘rgatitjes s‘ raportit', 
+                'Viti fiskal',
+                'Emri zyrtar i Autoritetit Kontraktues',
+                'Lloji i Autoritetit Kontraktues (zgjidh‘ne nj‘ren)',
+                'Kodi buxhetor',
+                'Adresa',
+                'Kodi postar - Qyteti',
+                'Tel. fiks/ Celulari /Faksi',
+                'Emri i menaxherit t‘ prokurimit',
+                'E-mail adresa',
+                'Adresa e webit t‘ AK',
+                ]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
+    writer.writeheader()
+    writer.writerow({
+                    'Data e p‘rgatitjes s‘ raportit':ws['G8'].value, 
+                    'Viti fiskal': ws['P8'].value,
+                    'Emri zyrtar i Autoritetit Kontraktues': ws['I12'].value,
+                    'Lloji i Autoritetit Kontraktues (zgjidh‘ne nj‘ren)':ws['I13'].value,
+                    'Kodi buxhetor':ws['I14'].value,
+                    'Adresa': ws['I15'].value,
+                    'Kodi postar - Qyteti': ws['I16'].value,
+                    'Tel. fiks/ Celulari /Faksi': ws['I17'].value,
+                    'Emri i menaxherit t‘ prokurimit': ws['I18'].value,
+                    'E-mail adresa': ws['I19'].value,
+                    'Adresa e webit t‘ AK' : ws['I20'].value
+                    })
 
-df.to_csv('2016 Gjilan public works report-releases.csv')
-meta_info.to_csv('2016 Gjilan public works report-meta info.csv')
-
-# convert file to json
-d = df.to_dict(orient='list')
-json_list = []
-
-for row in d:
-    json = {
-        "uri":"http://data.opendatakosovo.org/procurements/2016/gjilan-contract.json",
-        "publishedDate":"2014-07-21T14:45:00Z",
-        "publisher": {
-            "scheme":"OCSD",
-            "uid":"na",
-            "name": "Open Data Kosovo",
-            "uri": "http://data.opendatakosovo.org/procurements/"
-        },
-        "license":"http://opendatacommons.org/licenses/pddl/1.0/",
-        "publicationPolicy":"https://github.com/open-contracting/sample-data/",
-        "blah": {
-            "releases": [
-                {
-                    "planning":[
-                        {"period": {
-                            "endDate": row[7],
-                            },
-                        },
-                        {"budget": {
-                            "source": row[0]
-                            }
-                        }
-                    ]
-                },
-                {
-                    "tender": {
-                        "description": row[2],
-                        "value": {
-                            "description": row[3],
-                        },
-                        "procurementMethod": row[4],
-                        "title": row[6],
-                        "tenderPeriod": {
-                            "startDate": row[8],
-                            "endDate": row[9],
-                        }
-                    }
-                },
-                {
-                    "contract": {
-                        "dateSigned": row[10],
-                        "contractPeriod": {
-                            "timeframe": row[11],
-                            "endDate": row[12]
-                        },
-                        "contractValue": {
-                            "amount": {
-                                "estimate": row[13],
-                                "deductions": row[16]
-                            }
-                        }
-                    }
-                },
-                {"FFP": row[5]},
-                {"contractPrice": row[14]},
-                {
-                    "annex":{
-                        "annexValue":{
-                            "amount": row[15], 
-                        }
-                    }
-                }
-            ]
-        }
-    }
-    
-    json_list.push(json)
-
-print(json_list)
-
+#iterate through releases
+#for row in ws.iter_rows(min_row=28, max_col=3, max_row=138):
 
